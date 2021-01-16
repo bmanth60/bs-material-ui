e94294252c937484208da9a2a53f7b7a5bda81a2
bmanth60 <bman.brianthai@gmail.com>
2021-01-16 12:34:21 -0800

    Revert "Move bs-material-ui to root for npm to pull down latest."
    This reverts commit d93f44276e9bfac016a50be2fabe231ba53d7477.

--- build.sh
+++ build.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
-rm src/*
-cp ../../output/reason/*.re src
-yarn bs:build
-rm src/*.bs.js
+pwd=$(pwd)
+
+git pull
+rm -rf output
+
+cd tools/json-schema-parser
+yarn
+yarn build
+
+cd $pwd
+
+cd tools/component-extractor
+yarn
+yarn build
+
+cd $pwd
+
+cd tools/binding-generator
+yarn
+yarn build
+
+cd $pwd
+
+cd public/bs-material-ui-ppx
+yarn
+yarn build
+
+cd $pwd
---
+++ tools/binding-generator/src/render-colors.ts
@@ -0,0 +1,50 @@
+import * as Fs from 'fs';
+import * as Path from 'path';
+import {
+  capitalize,
+  isNumeric,
+  uncapitalize,
+} from './helpers/generate-reason-name';
+import outputDirectory from './output';
+
+interface ColorFile {
+  [colorName: string]: {
+    [colorCode: string]: string;
+  };
+}
+
+const GetColors = (): ColorFile =>
+  JSON.parse(
+    Fs.readFileSync(Path.join(outputDirectory, 'json', 'colors.json'), 'utf8'),
+  );
+
+const RenderColors = () => {
+  const colors = GetColors();
+  const colorDefinitions = Object.keys(colors).reduce((prev, colorName) => {
+    return [
+      `
+        type t${capitalize(colorName)} = {
+                ${Object.keys(colors[colorName])
+                  .map(
+                    (key) => `
+                      [@bs.as "${key}"] ${
+                      isNumeric(key) ? 'c' : ''
+                    }${uncapitalize(key)}: string,
+                  `,
+                  )
+                  .join('\n')}
+            };
+            [@bs.module "@material-ui/core/colors"] external ${colorName}: t${capitalize(
+        colorName,
+      )} = "${colorName}";
+        `,
+      ...prev,
+    ];
+  }, []);
+
+  return {
+    colorModule: colorDefinitions.join('\n'),
+  };
+};
+
+export default RenderColors();
